6e1bca193dcbf01d1d5b478f6dc92838c15951ca
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-10-11 03:11:24 -0400

    Updated GJK_Algo function, implemented containsOrigin function. Incorrect results.

--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -5,7 +5,7 @@
 bool GJK_Algo(const std::vector<Util::Vector>& polyA, const std::vector<Util::Vector>& polyB);
 Util::Vector supportFn(std::vector<Util::Vector> poly, Util::Vector d);
 std::vector<Util::Vector> MinDiff(std::vector<Util::Vector> polyA, std::vector<Util::Vector> polyB);
-std::tuple<std::vector<Util::Vector>, Util::Vector> containsOrigin(std::vector<Util::Vector> simplex, Util::Vector A);
+bool containsOrigin(std::vector<Util::Vector>& simplex, Util::Vector& d);
 
 SteerLib::GJK_EPA::GJK_EPA()
 {
@@ -17,6 +17,9 @@ bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector&
 	// Determine if the shapes intersect using GJK.
 	std::vector<Util::Vector> simplex;
 	bool hasIntersection;
+
+	return_penetration_depth = 0;
+	return_penetration_vector = Util::Vector();
 
 	hasIntersection = GJK_Algo(_shapeA, _shapeB);
 
@@ -33,72 +36,57 @@ bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector&
 /*** Note: The return value is currently set to bool but needs to return the simplex and bool when implementation is complete.*/
 bool GJK_Algo(const std::vector<Util::Vector> &polyA, const std::vector<Util::Vector> &polyB) {
 
-	// 1: Compute the Minkowski Difference of A and B
-	std::vector<Util::Vector> minDiff = MinDiff(polyA, polyB);
+	std::vector<Util::Vector> minDiff, W;
+	Util::Vector d, t, v, w;
 
-	// 2: Choose arbitrary point v
-	Util::Vector v;
+	// Get the Minkoswki Difference for polyA and polyB.
+	minDiff = MinDiff(polyA, polyB);	
+	
 	int length = minDiff.size();
 
-	if (length > 0) {
+	t = minDiff[std::rand() % length]; // Gets a random point within Minkowski Difference.
+	v = supportFn(minDiff, t); // Gets point on the boundary of the Minkowski Difference.
+	d = v.operator-();
 
-		v = minDiff[std::rand() % length];
+	W.push_back(v); // Start with one point in the simplex.
 
-	}
-
-	// 3: Define empty set W
-	std::vector<Util::Vector> W;
-
-	// Determine initial search direction vector.
-	Util::Vector d = v.operator*(-1);
-
-	// 4: Loop
 	while (true) {
 
-		// 5: Get support point w using support function.
-		Util::Vector w = supportFn(minDiff, d);
-		
-		// 6: If w approximate to v, then return
-		Util::Vector norm_w = w.operator/(w.lengthSquared());
-		Util::Vector norm_v = v.operator/(v.lengthSquared());
-		
-		/*** Probably needs to be changed. ***/
-		if (norm_w == norm_v) {
+		// Get support point using direction vector d.
+		w = supportFn(minDiff, d);
 
-			return true;
+		if (Util::dot(w, d) <= 0) {
 
-		}
-		else
-		{
-			// 7: Otherwise add w to W
+			return false;
+
+		} else {
+			
 			W.push_back(w);
 
-			// 8: If convex hull of W contains origin, shapes intersect. Return.
+			// Check if the simplex contains the origin.
+			if (containsOrigin(W, d)) {
+
+				return true;
+
+			}
 
 		}
 
-
-	/*To-Do:
-	 9: Otherwise find point in simplex closest to the origin.
-
-	 10: Remove points from W that do not contribute to the closest point approximation.
-
-	 11: Repeat until algorithm converges. */
 	}
-	return false;
 }
 
 Util::Vector supportFn(std::vector<Util::Vector> poly, Util::Vector d) {
 
-	float highest, dotProduct;
-	Util::Vector supportPoint;
-	Util::Vector currPoint;
+	float highest = -FLT_MAX;
+	float dotProduct;
+	Util::Vector supportPoint, currPoint;
+
 	int polyLength = poly.size();
 
 	for (int i = 0; i < polyLength; i++) {
 
 		currPoint = poly[i];
-		dotProduct = (currPoint.x * d.x) + (currPoint.z * d.z);
+		dotProduct = Util::dot(currPoint, d);
 
 		if (dotProduct > highest) {
 
@@ -142,4 +130,74 @@ std::vector<Util::Vector> MinDiff(std::vector<Util::Vector> polyA, std::vector<U
 
 	return minDiff;
 
+}
+
+//Takes the current simplex and determines if the origin is within it.
+bool containsOrigin(std::vector<Util::Vector>& simplex, Util::Vector& d) {
+
+	// Get the last point added to the simplex and compute A0.
+	Util::Vector A = simplex[simplex.size() - 1];
+	Util::Vector A0 = A.operator-();
+
+	// Check the type of simplex we have.
+
+	if (simplex.size() == 2) {
+
+		// Get the point added before A. Calculate AB.
+		Util::Vector B = simplex[0];
+		Util::Vector AB = B.operator-(A);
+
+		// Get the dot product of AB and A0.
+		float dotProd = Util::dot(AB, A0);
+
+		d = Util::cross(Util::cross(AB, A0), AB);
+
+		return false;
+
+	}
+	else if (simplex.size() == 3) {
+
+		// Get the other 2 points in the simplex.
+		Util::Vector B = simplex[1];
+		Util::Vector C = simplex[0];
+
+		Util::Vector AB = B.operator-(A);
+		Util::Vector AC = C.operator-(A);
+		Util::Vector ABCxAC = Util::cross(Util::cross(AB, AC), AC);
+		Util::Vector ABCxAB = Util::cross(Util::cross(AC, AB), AB);
+
+		if (Util::dot(ABCxAC, A0) > 0) {
+
+			// Remove B
+			simplex[1] = A;
+			simplex.pop_back();
+
+			d = Util::cross(Util::cross(AC, A0), AC);
+
+			return false;
+
+		}
+		else {
+
+			if (Util::dot(ABCxAB, A0) > 0) {
+
+				// Remove C from simplex.
+				simplex[0] = B;
+				simplex[1] = A;
+				simplex.pop_back();
+
+				d = Util::cross(Util::cross(AB, A0), AB);
+
+				return false;
+
+			}
+
+			return true;
+
+
+		}
+
+	}
+
+	return false;
 }
